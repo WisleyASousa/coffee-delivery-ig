1d4d08ed9c89061e00a55a66df3515a85ff273ca
WisleyASousa <wisleya.sousa@outlook.com.br>
2023-04-27 10:25:53 -0300

    finalizando estilizacao do navbar

--- src/components/Navbar/styles.ts
+++ src/components/Navbar/styles.ts
@@ -8,13 +8,28 @@ export const NavbarContainer = styled.div`
   align-items: center;
   justify-content: space-between;
 
+  img {
+    padding-top: .5rem;
+    padding-bottom: 1rem;
+  }
+
   nav {
     display: flex;
     align-items: center;
+    justify-content: center;
     gap: 0.75rem;
+    padding: .5rem 0;
+
     a {
       
 
+    }
+  }
+
+  @media (max-width: 425px){
+    flex-direction: column;
+    nav {
+      gap: 4rem;
     }
   }
 
@@ -27,17 +42,28 @@ export const BtnBase = styled.a `
   justify-content: center;
   cursor: pointer;
   border-radius: 8px;
+  border: 1px solid transparent;
+  
 `
 
 export const BtnLocation = styled(BtnBase) `
   background: ${props => props.theme['purpleLight']};
-  
-  color: ${props => props.theme['purpleDark']}
+  color: ${props => props.theme['purpleDark']};
 
+
+  &:hover {
+    border: 1px solid ${props => props.theme['purpleDark']};
+    transition: all 3s ease-in;
+
+  }
 
 `
 export const BtnCart = styled(BtnBase) `
   background: ${props => props.theme['yellowLight']};
   color: ${props => props.theme['yellowDark']};
+  &:hover {
+    border: 1px solid ${props => props.theme['yellowDark']};
+  }
+
 
 `
--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -1,11 +1,17 @@
 import styled from "styled-components";
 
 export const LayoutContainer = styled.div`
-  max-width: 70rem;
+  max-width: 60rem;
   height: 100vh;
   margin: 0 auto;
 
   display: flex;
   flex-direction: column;
 
+  @media (min-width: 768px) {
+    padding: 0 5rem;
+  }
+  @media (max-width: 425px) {
+    padding: 0.5rem;
+  }
 `
